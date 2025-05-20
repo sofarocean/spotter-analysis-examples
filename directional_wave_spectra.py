628a65077675e8f1d376f00ce4a8c137bd041032
Anna Savage <annasavage@Annas-MacBook-Pro-2.local>
2025-05-20 11:47:45 -0700

    documentation for directional_wave_spectra.py and restoring jupyter notebook away from dev work

--- directional_wave_spectra.py
+++ directional_wave_spectra.py
@@ -35,21 +35,21 @@ import typing
 
 @njit(fastmath=True)
 def mem2_directional_distribution(
-    lagrange_multiplier,
-    direction_increment,
-    twiddle_factors,
+    lambdas: np.ndarray,
+    direction_increment: np.ndarray,
+    twiddle_factors: np.ndarray,
 ) -> np.ndarray:
     """
     Given the solution for the Lagrange multipliers- reconstruct the directional
     distribution.
-    :param lagrange_multiplier: the lagrange multipliers
-    :param twiddle_factors: [sin theta, cost theta, sin 2*theta, cos 2*theta] as a 4 by ndir array
+    :param lambdas: the Lagrange multipliers used to find the local minima of the error function
+    :param twiddle_factors: [sin theta, cos theta, sin 2*theta, cos 2*theta] as a 4 by ndir array -- directional basis functions
     :param direction_increment: directional stepsize used in the integration, nd-array
     :return: Directional distribution array as a function of directions
     """
     inner_product = np.zeros(twiddle_factors.shape[1])
     for jj in range(0, 4):
-        inner_product = inner_product + lagrange_multiplier[jj] * twiddle_factors[jj, :]
+        inner_product = inner_product + lambdas[jj] * twiddle_factors[jj, :]
 
     inner_product = inner_product - np.min(inner_product)
 
@@ -57,24 +57,31 @@ def mem2_directional_distribution(
     return np.exp(-inner_product) * normalization
 
 def get_direction_increment(directions_radians: np.ndarray) -> np.ndarray:
-
+    """
+    :param directions_radians: direction array (in radians)
+    :return: directional increment for integrating (in radians)
+    """
     # Calculate the forward difference appending the first entry to the back
     # of the array. Use modular trickery to ensure the angle is in [-pi,pi]
-    forward_diff = (
-        np.diff(directions_radians, append=directions_radians[0]) + np.pi
-    ) % (2 * np.pi) - np.pi
+    forward_diff = (np.diff(directions_radians, append=directions_radians[0]) + np.pi) % (2 * np.pi) - np.pi
 
     # Calculate the backward difference prepending the last entry to the front
     # of the array. Use modular trickery to ensure the angle is in [-pi,pi]
-    backward_diff = (
-        np.diff(directions_radians, prepend=directions_radians[-1]) + np.pi
-    ) % (2 * np.pi) - np.pi
+    backward_diff = (np.diff(directions_radians, prepend=directions_radians[-1]) + np.pi) % (2 * np.pi) - np.pi
 
     # The interval we are interested in is the average of the forward and backward
     # differences.
     return (forward_diff + backward_diff) / 2
 
-def initial_value(a1: np.ndarray, b1: np.ndarray, a2: np.ndarray, b2: np.ndarray):
+def initial_value(a1: np.ndarray, b1: np.ndarray, a2: np.ndarray, b2: np.ndarray) -> np.ndarray:
+    """
+     Initializes guess for root finding using the directional moments
+    :param a1: first cosine directional moment
+    :param b1: first sine directional moment
+    :param a2: second cosine directional moment
+    :param b2: second sine directional moment
+    :return guess: initial guess for root finding using the directional moments
+    """
     guess = np.empty((*a1.shape, 4))
     fac = 1 + a1**2 + b1**2 + a2**2 + b2**2
     guess[..., 0] = 2 * a1 * a2 + 2 * b1 * b2 - 2 * a1 * fac
@@ -84,15 +91,21 @@ def initial_value(a1: np.ndarray, b1: np.ndarray, a2: np.ndarray, b2: np.ndarray
     return guess
 
 @njit(fastmath=True)
-def moment_constraints(lambdas, twiddle_factors, moments, direction_increment):
+def moment_constraints(lambdas: np.ndarray, twiddle_factors: np.ndarray, moments: np.ndarray, direction_increment: np.ndarray) -> np.ndarray:
+    """
+    This is the error function between the true directional moments and the discrete approximation. This is the function that we minimize (in root_finder below) in order to maximize entropy.
+    :param lambdas: Lagrange multipliers used to find the local minima of the error function
+    :param twiddle_factors: directional basis functions used to find the local minima of the error function
+    :param moments: directional moments
+    :param direction_increment: directional stepsize used in the integration
+    :return: error between estimated and true directional moments
+    """
     # Get the current estimate of the directional distribution
     dist = mem2_directional_distribution(lambdas, direction_increment, twiddle_factors)
     out = np.zeros(4)
     for mm in range(0, 4):
         # note - the part after the "-" is just a discrete approximation of the Fourier sine/cosine amplitude (moment)
-        out[mm] = moments[mm] - np.sum(
-            (twiddle_factors[mm, :]) * dist * direction_increment
-        )
+        out[mm] = moments[mm] - np.sum((twiddle_factors[mm, :]) * dist * direction_increment)
 
     return out
 
@@ -103,13 +116,20 @@ def root_finder(
     a2: typing.Union[np.ndarray, float],
     b2: typing.Union[np.ndarray, float],
 ) -> np.ndarray:
+    """
+    :param directions_radians: directional array (in radians)
+    :param a1: first cosine directional moment
+    :param b1: first sine directional moment
+    :param a2: second cosine directional moment
+    :param b2: second sine directional moment
+
+    :return: directional distribution array as a function of directions
+    """
 
     number_of_frequencies = a1.shape[-1]
     number_of_points = a1.shape[0]
 
-    directional_distribution = np.zeros(
-        (number_of_points, number_of_frequencies, len(directions_radians))
-    )
+    directional_distribution = np.zeros((number_of_points, number_of_frequencies, len(directions_radians)))
 
     direction_increment = get_direction_increment(directions_radians)
 
@@ -143,9 +163,7 @@ def root_finder(
             )
             lambdas = res.x
 
-            directional_distribution[ipoint, ifreq, :] = mem2_directional_distribution(
-                lambdas, direction_increment, twiddle_factors
-            )
+            directional_distribution[ipoint, ifreq, :] = mem2_directional_distribution(lambdas, direction_increment, twiddle_factors)
 
     return directional_distribution
 
@@ -165,7 +183,14 @@ def estimate_directional_spectrum(
     direction: np.ndarray,
 ) -> np.ndarray:
     """
+    :param e: variance density
+    :param a1: first cosine directional moment
+    :param b1: first sine directional moment
+    :param a2: second cosine directional moment
+    :param b2: second sine directional moment
+    :param direction: direction vector (in degrees)
 
+    :return: estimated 2D directional spectrum
     """
     # convert degrees to radians
     direction_radians = np.deg2rad(direction)
